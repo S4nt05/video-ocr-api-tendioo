244215beb00d162633f0fa57bffa3796da0baa8d
S4nt05 <60900178+S4nt05@users.noreply.github.com>
2025-09-15 10:31:34 -0600

    post upload router faltante

--- routes/posts.js
+++ routes/posts.js
@@ -5,6 +5,7 @@ const { uploadVideo, publishVideo } = require('../utils/tiktokApi');
 const path = require('path');
 const fs = require('fs');
 
+const router = express.Router();
 const uploadDir = path.join(__dirname, '../public/uploads/');
 if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir, { recursive: true });
 
